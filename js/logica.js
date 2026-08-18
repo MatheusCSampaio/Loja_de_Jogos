eab99d752e25f0b8d342faddc3036d3cf5e49a72
Matheus Cavalcante Sampaio <matheusampaio098@gmail.com>
2026-08-18 11:17:01 -0300

    Melhorando o carrinho

--- js/logica.js
+++ js/logica.js
@@ -1,4 +1,5 @@
 let quantidade = 0;
+let carrinho = [];
 
 const contador = document.getElementById("contador");
 const botoes = document.querySelectorAll(".comprar");
@@ -7,6 +8,11 @@ botoes.forEach(function(botao) {
     botao.addEventListener("click", function() {
         quantidade++;
         contador.textContent = quantidade;
+
+        const nomeJogo = botao.dataset.nome;
+        carrinho.push(nomeJogo);
+        atualizarCarrinho();
+        console.log(carrinho);
     });
 });
 
@@ -16,3 +22,27 @@ const modal = document.getElementById("carrinho-modal");
 carrinhoBtn.addEventListener("click", function(){
     modal.style.display = "block";
 });
+
+const listaCarrinho =
+document.getElementById("lista-carrinho")
+
+function atualizarCarrinho() {
+    listaCarrinho.innerHTML = "";
+
+    carrinho.forEach(function(jogo) {
+        listaCarrinho.innerHTML +=
+        `<p>${jogo}</p>`;
+    });
+}
+
+const fecharModal =
+document.getElementById("fechar-modal");
+
+fecharModal.addEventListener(
+    "click",
+    function() {
+
+        modal.style.display = "none";
+
+    }
+);
